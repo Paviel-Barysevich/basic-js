41f36cb9d994be49dac5b8fea4b021d254763f3e
Paviel Barysevich-Bleyaniuk <pavelmessage@gmail.com>
2025-01-03 01:43:43 +0300

    feat: add solution to mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = new Array(matrix.length).fill(0);
+
+  for (let i = 0; i < result.length; i++) {
+    const newRow = new Array(matrix[0].length).fill(0);
+
+    result[i] = newRow;
+  }
+
+  for (let row = 0; row < result.length; row++) {
+    for (let col = 0; col < result[0].length; col++) {
+      let cell = result[row][col];
+      const countPrevRow = (counter) => {
+        if (matrix[row - 1][col - 1]) counter++;
+        if (matrix[row - 1][col]) counter++;
+        if (matrix[row - 1][col + 1]) counter++;
+
+        return counter;
+      };
+      const countCurrentRow = (counter) => {
+        if (matrix[row][col + 1]) counter++;
+        if (matrix[row][col - 1]) counter++;
+
+        return counter;
+      };
+      const countNextRow = (counter) => {
+        if (matrix[row + 1][col - 1]) counter++;
+        if (matrix[row + 1][col]) counter++;
+        if (matrix[row + 1][col + 1]) counter++;
+
+        return counter;
+      };
+
+      if (matrix[row - 1] !== undefined &&
+          matrix[row + 1] !== undefined
+      ) {
+        cell = countPrevRow(cell);
+        cell = countCurrentRow(cell);
+        cell = countNextRow(cell);
+      }
+
+      if (matrix[row - 1] === undefined) {
+        cell = countCurrentRow(cell);
+        cell = countNextRow(cell);
+      }
+
+      if (matrix[row + 1] === undefined) {
+        cell = countPrevRow(cell);
+        cell = countCurrentRow(cell);
+      }
+
+      result[row][col] = cell;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
